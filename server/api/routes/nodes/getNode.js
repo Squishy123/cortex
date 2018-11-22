28033c10042ab9a4cf012c623b86f414783ff588
Christian Wang <christian.gnaw@gmail.com>
2018-11-22 11:53:52 -0500

    removed useless pre from getNode

--- server/api/routes/nodes/getNode.js
+++ server/api/routes/nodes/getNode.js
@@ -1,5 +1,3 @@
-const verifyAPIToken = require('../../middle/clusterFunctions').verifyAPIToken;
-
 const Node = require('../../models/node');
 
 const mongoose = require('mongoose');
@@ -9,11 +7,10 @@ const Boom = require('boom');
 module.exports = {
     method: 'GET',
     path: '/api/node',
-    pre:[verifyAPIToken],
     config: {
         handler: async(req, res) => {
             try {
-                await verifyAPIToken(req, res);
+
                 let nodes = await Node.find({api_token: req.headers.api_token, _id: mongoose.Types.ObjectId(req.headers.node_id)});
                 return res.send(nodes);
             } catch(err) {
