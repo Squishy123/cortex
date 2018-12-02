68948da3609dad6fd8a035218936c86a1d48434c
squishy123 <christian.gnaw@gmail.com>
2018-12-01 21:55:47 -0500

    Lowered meta amount and added openssl certs to gitignore

--- server/api/routes/meta/getMetas.js
+++ server/api/routes/meta/getMetas.js
@@ -19,9 +19,9 @@ module.exports = {
             //console.log(req.headers.node_id);
             let verified;
             if (req.headers.skip_length)
-                verified = await Meta.find({ node_id: mongoose.Types.ObjectId(req.headers.node_id), reading_name: req.headers.reading_name }).skip(Number(req.headers.skip_length)).limit(100).sort({ created_at: -1 });
+                verified = await Meta.find({ node_id: mongoose.Types.ObjectId(req.headers.node_id), reading_name: req.headers.reading_name }).skip(Number(req.headers.skip_length)).limit(50).sort({ created_at: -1 });
             else
-                verified = await Meta.find({ node_id: mongoose.Types.ObjectId(req.headers.node_id), reading_name: req.headers.reading_name }).limit(100).sort({ created_at: -1 });
+                verified = await Meta.find({ node_id: mongoose.Types.ObjectId(req.headers.node_id), reading_name: req.headers.reading_name }).limit(50).sort({ created_at: -1 });
             //console.log(verified);
             if (!verified)
                 return res.send(Boom.badRequest("Invalid Meta!!!"));
